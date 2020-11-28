48e4883933925104be29fc97733541f1567c3b67
rexxnor <patr.gerb@hotmail.com>
2020-11-28 19:29:54 +0100

    new version with better tiles

---
+++ deviantart_rpg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="deviantart_rpg" tilewidth="32" tileheight="32" tilecount="6300" columns="60">
+ <image source="tiles/deviantart_rpg.png" width="1920" height="3360"/>
+</tileset>
