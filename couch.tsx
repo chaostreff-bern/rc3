75374a4acc80ee7c93076fc83a2bf26012a53fcc
Marenu <Marenu@users.noreply.github.com>
2020-12-19 22:03:59 +0100

    remove old files

--- couch.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="couch" tilewidth="32" tileheight="32" tilecount="35" columns="7">
- <image source="tiles/couch.png" width="224" height="160"/>
-</tileset>
--- deviant_art_rustic.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="deviant_art_rustic" tilewidth="32" tileheight="32" tilecount="1496" columns="8">
- <image source="tiles/devianart_rustical.png" width="256" height="6000"/>
-</tileset>
