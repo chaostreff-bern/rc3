1119f9977b51b5ba674768c05639f03ff42c36d5
Roland Rytz <roland.rytz@gmail.com>
2020-12-21 18:46:33 +0100

    add lab

---
+++ draemmli.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="draemmli" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="draemmli.png" width="1024" height="1024"/>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="42" duration="200"/>
+   <frame tileid="74" duration="200"/>
+   <frame tileid="106" duration="200"/>
+   <frame tileid="138" duration="200"/>
+   <frame tileid="106" duration="200"/>
+   <frame tileid="74" duration="200"/>
+   <frame tileid="42" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="171" duration="200"/>
+   <frame tileid="139" duration="2000"/>
+   <frame tileid="107" duration="200"/>
+   <frame tileid="267" duration="200"/>
+   <frame tileid="235" duration="200"/>
+   <frame tileid="203" duration="200"/>
+   <frame tileid="267" duration="200"/>
+   <frame tileid="75" duration="200"/>
+   <frame tileid="43" duration="2000"/>
+   <frame tileid="11" duration="200"/>
+   <frame tileid="235" duration="200"/>
+   <frame tileid="203" duration="200"/>
+   <frame tileid="267" duration="200"/>
+   <frame tileid="235" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="172" duration="200"/>
+   <frame tileid="140" duration="2000"/>
+   <frame tileid="108" duration="200"/>
+   <frame tileid="204" duration="200"/>
+   <frame tileid="236" duration="200"/>
+   <frame tileid="268" duration="200"/>
+   <frame tileid="204" duration="200"/>
+   <frame tileid="76" duration="200"/>
+   <frame tileid="44" duration="2000"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="236" duration="200"/>
+   <frame tileid="204" duration="200"/>
+   <frame tileid="268" duration="200"/>
+   <frame tileid="236" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="129">
+  <animation>
+   <frame tileid="129" duration="5000"/>
+   <frame tileid="193" duration="200"/>
+   <frame tileid="129" duration="300"/>
+   <frame tileid="193" duration="100"/>
+   <frame tileid="129" duration="3000"/>
+   <frame tileid="193" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="162">
+  <animation>
+   <frame tileid="162" duration="100"/>
+   <frame tileid="194" duration="100"/>
+   <frame tileid="226" duration="100"/>
+   <frame tileid="258" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
