02ad2f38c816dd8d2bf3a15a8a775ef0a1e770d7
Marenu <Marenu@users.noreply.github.com>
2020-12-25 23:03:42 +0100

    add copyright to deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ mkdir dist
 tiled --export-map json bern.tmx dist/main.json
 sed -i 's/..\\\///g' dist/main.json
 find . -name \*.png -exec cp {} dist/ \;
+cp COPYRIGHT dist/
 
 git add dist && git commit -m "Update"
 git push origin --delete main
